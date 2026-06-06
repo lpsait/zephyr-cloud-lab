0f32fa621aad8053de221b8e7d650dc156873f5d
lpsait <laurence.sait23@gmail.com>
2026-06-06 09:31:26 +0100

    Add files via upload

---
+++ zephyr-sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'zephyr-v1';
+
+const STATIC_FILES = [
+  '/',
+  '/index.html',
+  '/zephyr.html',
+  '/zephyr-sprite.png',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(STATIC_FILES))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  if (!event.request.url.startsWith(self.location.origin)) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        if (response && response.status === 200) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request)
+        .then(cached => cached || caches.match('/index.html'))
+      )
+  );
+});
